ef6e3a371f65eab3eff5f9ad4137fd1c6fb3bab0
seungjunJeong <tmdwns7429@gmail.com>
2025-05-23 10:38:26 +0900

    html 수정
    html 수정

--- docker/app.js
+++ docker/app.js
@@ -3,32 +3,6 @@ const http = require('http');
 var app = express();
 const server = http.createServer(app).listen(80);
 
-const path = require('path');
-
-
-const mysql = require('mysql');
-
-const connection = mysql.createConnection({
-    host: 'mysql-service'
-    , port: 3306
-    , user: 'root'
-    , password: '0000'
-    , database: 'linux'
-});
-
 app.get('/', function (req, res) {
-  res.sendFile("postListPage.html", { root: __dirname });
+  res.sendfile("postListPage.html");
 });
-
-app.get('/getPostList', function (req, res) {
-  let result = "";
-  let selectQuery = `SELECT * FROM post`;
-  connection.query(selectQuery,
-      function(err, rows, fields) {
-          if (err) throw err;
-          res.send(rows);
-      }
-  )
-});
-
-console.log("Server is run")
